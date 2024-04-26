50cdea4da4694b76bad3226bd95f1000bedce3c9
Palak4620 <palakjaiswal00050@gmail.com>
2024-04-26 13:14:14 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,20 @@
 function longestSubstring(s) {
 
-
-    // Implementation of longestSubstring function
+    var letters = check.split("");
+    var max = 0;
+    var result = [];
+    for (var i = 0; i < letters.length; i++) {
+        var start = i
+        if (result.indexOf(letters[i]) === -1) {
+            result.push(letters[i])
+        } else {
+            i = i - 1
+            result = []
+        }
+        if (max === 0 || max < result.length) {
+            max = result.length
+        }
+    }
+    return max
 }
-
 module.exports = { longestSubstring };
